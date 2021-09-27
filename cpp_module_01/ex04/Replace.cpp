2cb589a8b11d7a0d4520eb13b95471c7e2c9f41e
tisantos <tisantos@bocals-iMac.local>
2021-09-27 16:46:26 +0100

    cpp_module_01 ex04 done

--- cpp_module_01/ex04/Replace.cpp
+++ cpp_module_01/ex04/Replace.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <fstream>
 
 void replace(std::string filename, std::string s1, std::string s2) {
 
+    std::fstream    file;
+    std::fstream    file_replaced;
+    char            buffer;
+    std::string     file_content;
 
+    file.open(filename);
+
+    if (file.is_open()) {
+        file_replaced.open("FILENAME.replace", std::ios::out);
+
+        while (file.get(buffer)) {
+            file_content += buffer;
+        }
+
+        int count;
+        while ((count = file_content.find(s1)) != -1) {
+            file_content.erase(count, s1.length());
+            file_content.insert(count, s2);
+        }
+
+        file_replaced << file_content;       
+    }
+    file.close();
+    file_replaced.close();
 
 }
--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -8,18 +8,18 @@ int main() {
 	std::string		s1;
 	std::string		s2;
 
-	while (filename.empty())
+	while (filename.empty()) {
+		std::cout << "Please insert the filename: ";
 		std::getline(std::cin, filename);
-	while (s1.empty())
+	}
+	while (s1.empty()) {
+		std::cout << "Please insert the string which will be replaced: ";
 		std::getline(std::cin, s1);
-	while (s2.empty())
+	}
+	while (s2.empty()) {
+		std::cout << "Please insert the string to replace: ";
 		std::getline(std::cin, s2);
+	}
 
 	replace(filename, s1, s2);
-
-	/*std::cout << filename << std::endl;
-	std::cout << s1 << std::endl;
-	std::cout << s2 << std::endl;*/
-
-
 }
